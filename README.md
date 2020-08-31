91b1798e390db17018ee513af25aefb0725bd54f
Jake <36766388+JakeNTech@users.noreply.github.com>
2020-08-31 16:28:14 +0100

    Add Numbers to the end
    Now there is an option to specify that you can add numbers to the end of the password

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Brute force the password to a zip file using a word list
 
 ## Usage
 `$ python3 brute.py -f <zip_file_path/name> -w <wordlist_path/name>`
+
+## Options
+-n, --number <max_Number>\
+Using this option will append a number (upto the max_number selected) to the end of the number, e.g:
+```
+password1
+password2
+...
+```
--- brute.py
+++ brute.py
@@ -6,6 +6,7 @@ def getargs():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-f", "--file", dest="input_file", help="Name of the zip file to open", metavar="<filename>",required=True)
 	parser.add_argument("-w", "--word-list", dest="word_list", help="Name of the word list to open", metavar="<filename>", required=True)
+	parser.add_argument("-n","--number",dest="number",help="Define the ammount of numbers to add to the end of the password e.g. 9999", metavar="<integer>",default="0")
 	return parser.parse_args()
 
 def extract(zip_file,password):
@@ -15,17 +16,36 @@ def extract(zip_file,password):
 	except:
 		return False
 
-def cracker(zipFile_name,passwordFile):
+def cracker(zipFile_name,passwordFile_name):
 	#Open Zip file using the name that was taken form the command line input
 	zipFile = zipfile.ZipFile(zipFile_name)
-	passwordFile = open(passwordFile)
+	passwordFile = open(passwordFile_name)
+	#Could load word file into array but that seems in-efficient
 	for line in passwordFile.readlines():
 		password = line.strip("\n")
 		if extract(zipFile,password) != False:
-			print("Password Found:", password)
-			exit(0)
+			print("Password Found: ", password)
+			exit()
 	print("Password not found!")		
+
+def cracker_with_numbers(zipFile_name,passwordFile_name,max_number):
+	max_number = int(max_number)
+	zipFile = zipfile.ZipFile(zipFile_name)
+	passwordFile = open(passwordFile_name)
+	for line in passwordFile.readlines():
+		password = line.strip("\n")
+		for i in range(0,max_number):
+			new_password = password + str(i)
+			if extract(zipFile,new_password) != False:
+				print("Password Found: ", new_password)
+				exit()
+	print("Password not found!")
 
 if __name__ == "__main__":
 	arguments = getargs()
-	cracker(arguments.input_file,arguments.word_list)
+	if arguments.number != "0":
+		print("-n added!")
+		cracker_with_numbers(arguments.input_file,arguments.word_list, arguments.number)
+	else:
+		print("No additional options added!")
+		cracker(arguments.input_file,arguments.word_list)
