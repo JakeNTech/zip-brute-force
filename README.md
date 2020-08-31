8112066a68d6c9a3c8d99c396ed0a8d234900d02
Jake <36766388+JakeNTech@users.noreply.github.com>
2020-08-31 17:54:22 +0100

    Added  Letters, Symbols and All
    This isn't very good but it might do what you need it to do...

--- README.md
+++ README.md
@@ -5,10 +5,29 @@ Brute force the password to a zip file using a word list
 `$ python3 brute.py -f <zip_file_path/name> -w <wordlist_path/name>`
 
 ## Options
--n, --number <max_Number>\
-Using this option will append a number (upto the max_number selected) to the end of the number, e.g:
+-n, --number <number_of_numbers>\
+Using this option will append a number (upto the max_number selected) to the end of the password, e.g:
 ```
-password1
-password2
+password11
+password12
 ...
 ```
+-l, --letters <number_of_chars>\
+Using this option will append letters (upto the max_number) to the end of the password, e.g:
+```
+passwordaa
+passwordab
+...
+```
+-s, --symbols <number_of_chars>\
+Using this option will append symbols (upto the max_number) to the end of the password e.g:
+```
+password!!
+password!"
+...
+```
+-a , --all <number_of_chars>\
+Using this optin will apped Letter, Numbers and Symbols to the end of a password.
+
+
+Proberbly not the best/most useful script in the world, but it might do what you want it to do
--- brute.py
+++ brute.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 import argparse
 import zipfile
+import itertools
+import string
 
 def getargs():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-f", "--file", dest="input_file", help="Name of the zip file to open", metavar="<filename>",required=True)
 	parser.add_argument("-w", "--word-list", dest="word_list", help="Name of the word list to open", metavar="<filename>", required=True)
 	parser.add_argument("-n","--number",dest="number",help="Define the ammount of numbers to add to the end of the password e.g. 9999", metavar="<integer>",default="0")
+	parser.add_argument("-l","--letters",dest="letters",help="Add letters to the end of a password, number specifies how many letters",metavar="<integer>", default="0")
+	parser.add_argument("-s","--symbols",dest="symbols",help="Add symbols to the end of a password, number specifies how many symbols",metavar="<integer>",default="0")
+	parser.add_argument("-a","--all",dest="all",help="Add letters, numbers and sybmbols to the end of words ",metavar="<integer>",default="0")
 	return parser.parse_args()
 
 def extract(zip_file,password):
@@ -41,10 +46,67 @@ def cracker_with_numbers(zipFile_name,passwordFile_name,max_number):
 				exit()
 	print("Password not found!")
 
+def cracker_with_letters(zipFile_name,passwordFile_name,max_letters):
+	max_letters = int(max_letters)
+	chars = string.ascii_letters
+	zipFile = zipfile.ZipFile(zipFile_name)
+	passwordFile = open(passwordFile_name)
+	for line in passwordFile.readlines():
+		password = line.strip("\n")
+		for guess in itertools.product(chars, repeat=max_letters):
+			guess = ''.join(guess)
+			guess = password + guess
+			if extract(zipFile,guess) != False:
+				print("Password Found: ", guess)
+				exit()
+	print("Password not found!")
+
+def cracker_with_symbols(zipFile_name,passwordFile_name,max_symbols):
+	max_letters = int(max_symbols)
+	chars = string.punctuation
+	zipFile = zipfile.ZipFile(zipFile_name)
+	passwordFile = open(passwordFile_name)
+	for line in passwordFile.readlines():
+		password = line.strip("\n")
+		for guess in itertools.product(chars, repeat=max_letters):
+			guess = ''.join(guess)
+			guess = password + guess
+			if extract(zipFile,guess) != False:
+				print("Password Found: ", guess)
+				exit()
+	print("Password not found!")
+
+def all(zipFile_name,passwordFile_name,max_symbols):
+	max_letters = int(max_symbols)
+	chars = string.printable
+	zipFile = zipfile.ZipFile(zipFile_name)
+	passwordFile = open(passwordFile_name)
+	for line in passwordFile.readlines():
+		password = line.strip("\n")
+		for guess in itertools.product(chars, repeat=max_letters):
+			guess = ''.join(guess)
+			guess = password + guess
+			if extract(zipFile,guess) != False:
+				print("Password Found: ", guess)
+				exit()
+	print("Password not found!")
+
 if __name__ == "__main__":
 	arguments = getargs()
-	if arguments.number != "0":
-		print("-n added!")
+	if arguments.number != "0" and arguments.letters != "0" and arguments.symbols != "0" and arguments.all != "0":
+		print("Sorry! Only one option at a time can be used!")
+	elif arguments.letters != "0":
+		print("-l Option Selected!")
+		cracker_with_letters(arguments.input_file,arguments.word_list, arguments.letters)
+	elif arguments.symbols != "0":
+		print("-s Option selected!")
+		cracker_with_symbols(arguments.input_file,arguments.word_list, arguments.symbols)
+	elif arguments.all != "0":
+		print("-a Option selected...")
+		print("Good luck!")
+		all(arguments.input_file,arguments.word_list, arguments.all)
+	elif arguments.number != "0":
+		print("-n Option Selected!")
 		cracker_with_numbers(arguments.input_file,arguments.word_list, arguments.number)
 	else:
 		print("No additional options added!")
